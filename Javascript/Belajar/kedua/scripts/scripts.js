f0dddcddf6ed42b31ff1f1ab21b4294e271d5523
AD-TECH75 <muhammadabidhafizalauddin6@gmail.com>
2025-08-25 19:39:42 +0700

    menambahkan file belajar baru javascript

---
+++ Javascript/Belajar/kedua/scripts/scripts.js
@@ -0,0 +1,31 @@
+// untuk menampilkan tulisan di halaman web
+document.writeln("<p>Hello World coment in one line</p>");
+
+/* untuk menampilkan tulisan di halaman web
+   dengan komentar lebih dari satu baris */
+document.writeln("<p>Hello World comment in more line</p>");
+
+// belajar variabel
+document.writeln("Hello World"); // tipe data string
+document.writeln("<br>"); // untuk ganti baris
+document.writeln(100); // tipe data number
+document.writeln("<br>");
+document.writeln(3.14); // tipe data float
+document.writeln("<br>");
+document.writeln(5 + 5); // operasi matematika
+document.writeln("<br>");
+document.writeln(0xff); // tipe data hexadecimal
+document.writeln("<br>");
+document.writeln(true); // tipe data boolean true
+document.writeln("<br>");
+document.writeln(false); // tipe data boolean false
+document.writeln("<br>");
+document.writeln(0b100111); // tipe data binary
+document.writeln("<br>");
+document.writeln(0o123); // tipe data octal
+document.writeln("<br>");
+document.writeln("100" + 100); // tipe data string dan number
+document.writeln("<br>");
+document.writeln(
+	"hasil 1+1" + "= " + (1 + 1)
+); /* tanda tambah dapat digunakan untuk menggabungkan string atau untuk operasi matematika */
